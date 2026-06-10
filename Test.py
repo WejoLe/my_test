1c9f1b4318c2eed25484ebf23ef0685602a4fca1
WejoLe <Zekagold1@mail.ru>
2026-06-10 17:44:50 +0500

    В процессе случайных вопросов и ответов

--- Test.py
+++ Test.py
@@ -1,43 +1,28 @@
 import tkinter as tk
 import random
+import json
 
 root = tk.Tk()
 root.title("Тест")
 root.geometry("600x600")
 
 selected_answer = tk.IntVar()
-
-#Храненилище вопросов и их ответов в СЛУЧАЙНОМ пордке :)
-current_question = random.choice([
-    {
-    'text': 'Кому можно разглашать пароль от своего рабочего компьютера?',
-    'answers': ['Никому', 'Только отделу ИБиИТ', 'Всем'],
-    'correct': 1
-    },
-
-    {
-    'text': 'Кому можно?',
-    'answers': ['Мне','Им'],
-    'correct': 0
-    }
-])
-
-#Переменные для храниения значений
-question_text = current_question['text']
-all_answers = current_question['answers']
-correct_index = current_question['correct']
+selected_answer.set(-1)
 
 #Логика на нахождение вопроса из файла "Question"
-'''
-with open ('Questions', 'r', encoding='utf-8') as file:
-    question = file.readlines()
-    for line in question:
-        if not line.startswith('№') and line.strip() != '':
-            questions.append(line)
-'''
+with open ('questions.json', 'r', encoding='utf-8') as file:
+    question = json.load(file)
+
+corrent_question = random.choice(question)
+
+#Переменные для храниения значений
+question_text = corrent_question['text']
+all_answers = corrent_question['answers']
+correct_index = corrent_question['correct']
+question_number = 0
 
 #Вывод вопроса
-lbl_questions = tk.Label(root, text=question_text)
+lbl_questions = tk.Label(root, text=f'Вопрос\n\n {question_text}')
 lbl_questions.grid(row=0, column=0, columnspan=2, pady=50)
 
 #Логика на создание кнопки
@@ -45,19 +30,38 @@ for idx, ans_text in enumerate(all_answers):
     radio = tk.Radiobutton(root, text=ans_text, value=idx, variable=selected_answer)
     radio.grid(row=idx + 1, column=0, pady=10, sticky=tk.W)
 
+def load_next_question():
+    global question_number
+    question_number += 1
+    new_question_r = random.choice(question)
+
+    question_text = new_question_r['text']
+    all_answers = new_question_r['answers']
+    correct_index = new_question_r['correct']
+    
+    lbl_questions.config(text=f'Вопрос {question_number + 1}\n\n {question_text}')
+    radio.grid_forget
+
 #Логика на поверку ответа
+'''
 def check_answer():
     if selected_answer.get() == correct_index:
         lbl_result.config(text='Ответ правильный!')
     else:
         lbl_result.config(text='Ответ неправильный!')
 
+    selected_answer.set(-1)
+'''
+
+#Чтение сколько вопросов в хранилище
+Go = len(all_answers)
+
 #Кнопка для проверки
-btn = tk.Button(root, text='Ответить', command=check_answer)
-btn.grid(row=5, column=0, pady=20)
+btn = tk.Button(root, text='Ответить', command=load_next_question)
+btn.grid(row=Go + 2, column=0, pady=20)
 
 #Скрытый Label который отображает правильность ответа при его проверки
 lbl_result = tk.Label(root, text='')
-lbl_result.grid(row=6, column=0, pady=10)
+lbl_result.grid(row=Go + 3, column=0, pady=10)
 
 root.mainloop()
